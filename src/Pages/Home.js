367fa11a93512060759e7ba99ad2db5ab9ee87f1
12C0DE <rubenhnt@gmail.com>
2020-08-04 20:09:58 -0500

    retrieving user fname

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Fbase from '../Firebase/base';
+import { AuthContext } from '../Firebase/Auth';
 
 export const Home = () => {
+	function GetName() {
+		const { currentUser } = useContext(AuthContext);
+		const [ fname, setFname ] = useState(null);
+
+		useEffect(
+			() => {
+				Fbase.firestore().collection('users').where('uid', '==', currentUser.uid).onSnapshot((dt) => {
+					const user = dt.docs.filter((doc) => doc.data).map((doc) => ({
+						id: doc.id,
+						...doc.data()
+					}));
+					console.log('ran getName()');
+					setFname(user[0].fname);
+				});
+			},
+			[ currentUser.uid ]
+		);
+		return fname;
+	}
+
+	const userFname = GetName();
+
 	return (
 		<div>
-			<h1>Home</h1>
-			<button onClick={() => Fbase.auth().signOut()}>Sign out</button>
+			<div>
+				<h1>Home</h1>
+			</div>
+			<div>
+				<h2>Welcome {userFname}</h2>
+			</div>
+			<div>
+				<button onClick={() => Fbase.auth().signOut()}>Sign out</button>
+			</div>
 		</div>
 	);
 };
