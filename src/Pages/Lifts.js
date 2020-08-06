9a71f1da85cb7a76146a44ac82a4b8c7bf7b2ce6
12_Mac <rubenhnt@gmail.com>
2020-08-06 00:51:44 -0500

    adding axios calls

--- src/Pages/Lifts.js
+++ src/Pages/Lifts.js
@@ -7,7 +7,18 @@ export const Lifts = () => {
     return (
         <div>
             <h1>Lifts</h1>
+            <div>
+                <h2>Muscle groups</h2>
 <NameFetching url="https://wger.de/api/v2/exercisecategory/?status=2" />
+            </div>
+            <div>
+                <h2>Equipment</h2>
+                <NameFetching url='https://wger.de/api/v2/equipment/?status=2' />
+            </div>
+            <div>
+                <h2>Muscles</h2>
+                <NameFetching url='https://wger.de/api/v2/muscle/?status=2' />
+            </div>
 <Link to='/'>Back Home</Link>
         </div>
     );
