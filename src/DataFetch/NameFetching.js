31ec98d15fa1a2386a4aadb9f04a4b4862eaa5f1
12_Mac <rubenhnt@gmail.com>
2020-08-08 00:34:22 -0500

    adding exercise data to exercise page

--- src/DataFetch/NameFetching.js
+++ src/DataFetch/NameFetching.js
@@ -1,52 +1,72 @@
-import React, { useEffect, useContext, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { GlobalContext } from '../Context/GlobalState';
+import React, { useEffect, useContext, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { GlobalContext } from "../Context/GlobalState";
 
 function NameFetching(props) {
-	const [ posts, setPosts ] = useState([]);
-	const { IsLoading, selectItem, showCategory } = useContext(GlobalContext);
+  const [posts, setPosts] = useState([]);
+  const {
+    IsLoading,
+    selectItem,
+    setExDesc,
+    setExEquip,
+    setExName,
+    setMuscles,
+    setMuscles2nd,
+    showCategory
+  } = useContext(GlobalContext);
 
-	useEffect(
-		() => {
-			IsLoading(true);
-			axios
-				.get(props.url)
-				.then((res) => {
-					setPosts(res.data);
-					IsLoading(false);
-				})
-				.catch((err) => {
-					console.log(`err: ${err}`);
-					IsLoading(false);
-				});
-		},
-		[ props.url ]
-	);
+  useEffect(() => {
+    IsLoading(true);
+    axios
+      .get(props.url)
+      .then(res => {
+        setPosts(res.data);
+        IsLoading(false);
+      })
+      .catch(err => {
+        console.log(`err: ${err}`);
+        IsLoading(false);
+      });
+  }, [props.url]);
 
-	function setCategoryValues(itemId, category) {
-		selectItem(itemId);
-		showCategory(category);
-	}
+  //   function setExValues(itemId, category) {
+  //     selectItem(itemId);
+  //     showCategory(category);
+  //   }
+  function setExValues(post) {
+    selectItem(+post.id);
+    showCategory(props.category);
 
-	return (
-		<div>
-			<ul>
-				{posts.map((post) => (
-					<li key={post.id}>
-						<Link
-							to={props.path}
-							onClick={() => {
-								setCategoryValues(+post.id, props.category);
-							}}
-						>
-							{post.name}
-						</Link>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+    if (props.path === "/exercise") {
+      setExDesc(post.description);
+      setExEquip(post.equipment);
+      setExName(post.name);
+      setMuscles(post.muscles);
+      setMuscles2nd(post.muscles_secondary);
+    }
+  }
+
+  return (
+    <div>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <Link
+              to={props.path}
+              onClick={() => {
+                //     setExValues(+post.id, props.category);
+                //   }}
+                setExValues(post);
+              }}
+            >
+              {post.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default NameFetching;
--- src/Pages/Exercise.js
+++ src/Pages/Exercise.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { GlobalContext } from "../Context/GlobalState";
 
 export const Exercise = () => {
-	return (
-		<div>
-			<h1>Exercise</h1>
-			<Link to="/lifts">Back to Lifts</Link>
-		</div>
-	);
+  const {
+    exDescription,
+    exEquip,
+    exName,
+    muscles,
+    muscles2nd,
+    selectedItem
+  } = useContext(GlobalContext);
+  return (
+    <div>
+      <h1>Exercise</h1>
+      <div>
+        <h2>Name: {exName}</h2>
+        <h2>Desc: {exDescription}</h2>
+        <h2>Equipment: {exEquip}</h2>
+        <h2>Muscles: {muscles.map(musc => `${musc}, `)}</h2>
+        <h2>2nd Muscles: {muscles2nd.map(musc2 => `${musc2}, `)}</h2>
+      </div>
+      <Link to="/lifts">Back to Lifts</Link>
+    </div>
+  );
 };
