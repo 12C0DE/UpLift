e3c69ababb3f406bb89c3f761657e6e21dcc8078
Ruben Hernandez <rhernandez@weavix.com>
2026-08-15 19:26:51 -0500

    chore: configure Jest to run in-band to prevent memory issues in CI and local environments

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "jest-expo",
+  maxWorkers: 1,
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@shopify/flash-list|drizzle-orm)",
   ],
